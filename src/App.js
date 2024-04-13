38463c157e327b2b63ff76d3b431afa52c52eccb
Anton <imanton2@gmail.com>
2024-04-13 19:02:28 +0300

    anton3

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ export default function ProfilePage() {
                 <p className="text-muted mb-4">Center, Isreal</p>
                 <div className="d-flex justify-content-center mb-2">
                   <MDBBtn>Follow</MDBBtn>
-                  <MDBBtn onClick={() => window.open(CV, '_blank')} outline className="ms-1">Message</MDBBtn>
+                  <MDBBtn onClick={() => window.open(CV, '_blank')} outline className="ms-1">My CV</MDBBtn>
                 </div>
               </MDBCardBody>
             </MDBCard>
