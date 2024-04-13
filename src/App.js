0dd1c281ad75e416ef3cb2a58fa60c6e6f4e2fe1
Anton <imanton2@gmail.com>
2024-04-13 13:17:54 +0300

    added my svg

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ export default function ProfilePage() {
             <MDBCard className="mb-4">
               <MDBCardBody className="text-center">
                 <MDBCardImage
-                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
+                  src="%PUBLIC_URL%/favicon.ico"
                   alt="avatar"
                   className="rounded-circle"
                   style={{ width: '150px' }}
